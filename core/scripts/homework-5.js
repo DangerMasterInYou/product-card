f41f14026d05882070f2a4cabadd455f26af7254
Bakh <imoddinov@gmail.com>
2026-02-26 07:29:28 +0500

    fix: corrected errors and refactor code

--- core/scripts/homework-5.js
+++ core/scripts/homework-5.js
@@ -1,20 +1,13 @@
 /*1. Create a function that takes 2 parameters:
 city and temperature and outputs a message to the console "The temperature is currently at X degrees Celsius"
 */
-function nowWeather(city, temperature) {
+function currentWeather(city, temperature) {
   console.log(
     `Сейчас в ${city} температура  — ${temperature} градусов по Цельсию`,
   );
 }
 
-const nowWeatherButton = document.getElementById("now-weather-button");
-
-nowWeatherButton.addEventListener("click", () =>
-  nowWeather(
-    (city = document.getElementById("city-input").value),
-    (temperature = document.getElementById("temperature-input").value),
-  ),
-);
+currentWeather("СПб", 20);
 
 /*2. Create a variable that stores the speed of light inside itself (Google).
 Create a function that takes 1 argument - speed, a check is performed inside the function:
@@ -25,18 +18,18 @@ if equal to, the "Speed of light"
 const LIGHT_SPEED = 299792458;
 
 function comparisonSpeed(speed) {
-  speed > LIGHT_SPEED
-    ? console.log("Сверхсветовая скорость")
-    : speed < LIGHT_SPEED
-      ? console.log("Субсветовая  скорость")
-      : console.log("Скорость света");
+  if (speed > LIGHT_SPEED) {
+    console.log(`Сверхсветовая скорость `);
+  } else if (speed < LIGHT_SPEED) {
+    console.log(`Субсветовая скорость`);
+  } else {
+    console.log(`Скорость света`);
+  }
 }
 
-const lightSpeedButton = document.getElementById("light-speed-button");
-
-lightSpeedButton.addEventListener("click", () =>
-  comparisonSpeed((speed = document.getElementById("speed-input").value)),
-);
+comparisonSpeed(123);
+comparisonSpeed(12345678910);
+comparisonSpeed(299792458);
 
 /*3. Create variable #1, which contains the product, and variable #2, which contains its price (at your discretion).
 Next, we create a function that takes 1 parameter - the current budget, and a check takes place inside the function:
@@ -48,16 +41,14 @@ const nameProduct = "Macaroni";
 const price = 500;
 
 function buyProduct(budget) {
-  budget > price
+  budget >= price
     ? console.log(`${nameProduct} приобретён. Спасибо за покупку!`)
-    : console.log(`Вам не хватает ${budget - price}, пополните баланс`);
+    : console.log(`Вам не хватает ${price - budget}$, пополните баланс`);
 }
 
-const buyProductButton = document.getElementById("buy-product-button");
-
-buyProductButton.addEventListener("click", () =>
-  buyProduct((budget = document.getElementById("budget-input").value)),
-);
+buyProduct(499);
+buyProduct(500);
+buyProduct(501);
 
 /*4. Create 1 function and name it as you wish
 5. Create 3 variables (no matter which ones) and name them as you see fit.*/
@@ -69,6 +60,4 @@ function sumThreeRandomDigits() {
   console.log(`Сумма цифр: ${firstDigit + secondDigit + thirdDigit}`);
 }
 
-const sumDigitsButton = document.getElementById("sum-digits-button");
-
-sumDigitsButton.addEventListener("click", () => sumThreeRandomDigits());
+sumThreeRandomDigits();
